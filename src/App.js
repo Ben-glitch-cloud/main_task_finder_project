2c5d425ab384d71c5f24605f28739e034752be36
Ben-glitch-cloud <BTLawrence44@gmail.com>
2023-03-29 22:18:00 +0100

    data now being fetech from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React, { useEffect, useState } from "react"
 import './App.scss';
 
 function App() {
+
+  const [data, setData] = useState([])
+  // const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http://www.boredapi.com/api/activity/")
+      const res = await result.json() 
+      setData([...data, res])
+      // setLoading(loading => !loading)
+    }
+    fetchData()
+  }, [])
+
+  console.log(data)
+  
+
   return (
     <div className="App">
       <h1>Hello World</h1>
