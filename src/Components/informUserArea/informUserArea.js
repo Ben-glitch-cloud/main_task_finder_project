5d14a1d5ea32ede9718f60a802df187171f227be
Ben-glitch-cloud <BTLawrence44@gmail.com>
2023-03-30 15:57:33 +0100

    form and information being added

--- src/Components/informUserArea/informUserArea.js
+++ src/Components/informUserArea/informUserArea.js
@@ -3,7 +3,43 @@ import "../../styles/layout/_informUserAreaLayout.scss";
 function InformUserArea(){
     return(
         <div className="firstAppItemArea">
-            <h1>Work in progress</h1>
+            <div className="informationContaner">
+                <h1>Are you Bored?</h1>
+                <h2>Well Don't Be</h2>
+                <p>Find your next task by clicking on the activity button.</p>
+                <h2>Filter</h2>
+                <p>You don't want to receive a task that you can not do. So choose your filters.</p>
+                <form>
+                    <label>Participants</label>
+                    <select>
+                        <option>Null</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                    </select>
+                </form>
+
+                <div>
+                    <label>Accessibility</label>
+                    <select>
+                        <option>Null</option>
+                        <option>0.1</option>
+                        <option>0.2</option>
+                        <option>0.3</option>
+                        <option>0.4</option>
+                        <option>0.5</option>
+                        <option>0.6</option>
+                        <option>0.7</option>
+                        <option>0.8</option>
+                        <option>0.9</option>
+                        <option>1</option>
+                    </select>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/Components/showTask/showTask.js
+++ src/Components/showTask/showTask.js
@@ -2,39 +2,42 @@ import React, { useEffect, useState } from "react"
 
 function ShowTask(){
 
-const [data, setData] = useState([])
-const [loading, setLoading] = useState(false)
+// const [data, setData] = useState([])
+// const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch("http://www.boredapi.com/api/activity/")
-      const res = await result.json() 
-      setData([...data, res])
-      setLoading((loading) => true)
-    }
-    fetchData()
-  }, [])
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await fetch("http://www.boredapi.com/api/activity/")
+//       const res = await result.json() 
+//       setData([...data, res])
+//       setLoading((loading) => true)
+//     }
+//     fetchData()
+//   }, [])
+
+
+// {data.map((task) => {
+//   return <div> 
+//           <p>Activity : {task['activity']}</p>
+//           <p>Accessibility : {task['accessibility']}</p>
+//           <p>Type : {task['type']}</p>
+//           <p>{task['participants']}</p>
+//           <p>Price {task['price']}</p>
+//           <p>Link : {task['link']}</p>
+//           <p>key : {task['key']}</p>
+//       </div>
+// })}
+
+  function Message(){
+    console.log('hello')
+  }
   
-  if(loading){
     return(
-            <div>
-                {data.map((task) => {
-                    return <div> 
-                            <p>Activity : {task['activity']}</p>
-                            <p>Accessibility : {task['accessibility']}</p>
-                            <p>Type : {task['type']}</p>
-                            <p>{task['participants']}</p>
-                            <p>Price {task['price']}</p>
-                            <p>Link : {task['link']}</p>
-                            <p>key : {task['key']}</p>
-                        </div>
-                })}
-            </div>
+           <div className="secondAppItemArea">
+              <button className="findBtutton" onClick={Message}>Find Activity</button>
+           </div>
         )
-    } else {
-        return <p>Loading</p>
-    }
-
+  
 }
 
 export default ShowTask;
